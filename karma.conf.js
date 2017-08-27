724c7c354151742ea13da4e7e5cb2403f7b8cb12
Joacim Boive <joacim.boive@gmail.com>
2017-08-27 08:17:07 +0200

    Code coverage and bundle analyzer setup!

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,44 @@
-const webpackConfig = require('./webpack.config');
-const fileGlob = 'src/**/*.test.js';
+process.env.BABEL_ENV = 'test';
+const webpackEnv = {test: true};
+const webpackConfig = require('./webpack.config.babel')(webpackEnv);
+
+const testGlob = 'src/**/*.test.js';
+const srcGlob = 'src/**/!(*.test|*.stub).js';
 
 module.exports = (config) => {
-    config.set({
-        basePath: '',
-        frameworks: ['mocha', 'chai'],
-        files: [fileGlob],
-        preprocessors: {
-            [fileGlob]: ['webpack']
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [testGlob, srcGlob],
+    exclude: ['src/bootstrap.js'],
+    preprocessors: {
+      [testGlob]: ['webpack'],
+      [srcGlob]: ['webpack'],
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {noInfo: true},
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      check: {
+        global: {
+          statements: 11,
+          branches: 0,
+          functions: 0,
+          lines: 11,
         },
-        webpack: webpackConfig,
-        webpackMiddleware: {noInfo: true},
-        reporters: ['progress', 'mocha'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['Chrome'],
-        singleRun: false,
-        concurrency: Infinity,
-    });
+      },
+      reporters: [
+        {type: 'lcov', dir: 'coverage/', subdir: '.'},
+        {type: 'json', dir: 'coverage/', subdir: '.'},
+        {type: 'text-summary'},
+      ],
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Firefox'],
+    singleRun: true,
+    concurrency: Infinity,
+  });
 };
---
+++ webpack.config.babel.js
@@ -0,0 +1,94 @@
+/* eslint no-console:"off" */
+const {resolve} = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {getIfUtils, removeEmpty} = require('webpack-config-utils');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const OfflinePlugin = require('offline-plugin/runtime').install();
+
+module.exports = (env) => {
+    const {ifProd, ifNotProd} = getIfUtils(env);
+    const config = {
+        context: resolve('src'),
+        entry: './js/index/index.js',
+        output: {
+            filename: ifProd('bundle.[name].[chunkhash].js', 'bundle.[name].js'),
+            path: resolve('dist'),
+            pathinfo: ifNotProd()
+        },
+        devtool: ifProd('source-map', 'eval'),
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /(node_modules)/
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                },
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('css-loader')
+                    // loaders: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.html$/,
+                    use: {
+                        loader: 'html-loader'
+                    }
+                },
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+                    query: {
+                        name: 'static/media/files/[name].[hash:8].[ext]'
+                    }
+                }, {
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'file-loader',
+                    query: {
+                        name: 'static/media/fonts/[name].[hash:8].[ext]'
+                    }
+                },
+                {
+                    test: /\.(gif|jpe?g|png)$/,
+                    loader: 'url-loader?limit=25000',
+                    query: {
+                        limit: 10000,
+                        name: 'static/media/images/[name].[hash:8].[ext]'
+                    }
+                }
+            ]
+        },
+        plugins: removeEmpty([
+            new ProgressBarPlugin(),
+            new ExtractTextPlugin(ifProd('styles.[name].[chunkhash].css', 'styles.[name].css')),
+            ifProd(new InlineManifestWebpackPlugin()),
+            // ifProd(new webpack.optimize.CommonsChunkPlugin({
+            //     names: ['manifest']
+            // })),
+            new HtmlWebpackPlugin({
+                template: './index.html'
+                // inject: 'head'
+            }),
+            // new OfflinePlugin(),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: ifProd('"production"', '"development"')
+                }
+            }),
+            new BundleAnalyzerPlugin()
+        ])
+    };
+    if (env.debug) {
+        console.log(config);
+        debugger // eslint-disable-line
+    }
+    return config;
+};

eeb979d1181cf591cc092a6e29a565c25ab49c1e
Joacim Boive <joacim.boive@gmail.com>
2017-08-27 08:25:08 +0200

    Code coverage and bundle analyzer setup!

---
+++ src/js/index/index.js
@@ -0,0 +1,23 @@
+'use strict';
+if (module.hot) {
+    module.hot.accept();
+}
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+import EventEmitter from 'eventemitter3';
+
+import { typeahead } from '../modules/typeahead/typeahead';
+import { table } from '../modules/table/table';
+import { graph } from '../modules/graph/graph';
+
+const init = () => {
+    const EE = new EventEmitter();
+
+    typeahead.init(EE);
+    table.init(EE);
+    graph.init(EE);
+};
+
+init();
